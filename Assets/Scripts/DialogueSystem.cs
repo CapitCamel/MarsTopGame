058a036337f4579384d9da22313cab650a5997c6
yalunar1 <yalunar1@gmail.com>
2018-05-13 23:13:07 +0300

    Dialogue and movement fix

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour {
+
+    public string[] dialogueText;
+    public string openText;
+    GameManeger GM;
+    public bool open;
+    public bool openT;
+    private int i;
+    public float timeDelay;
+    float timer;
+    bool enter;
+
+	// Use this for initialization
+	void Start () {
+        GM = GameObject.Find("GameManegger").GetComponent<GameManeger>();
+        timer = timeDelay;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameManeger.openDialogue && open)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+
+            if (timer <= 0)
+            {
+                timer = 0;
+                if (i <= dialogueText.Length)
+                {
+                    i++;
+                    timer = timeDelay;
+                }
+                if (i == dialogueText.Length)
+                {
+                    open = false;
+                    GameManeger.openDialogue = false;
+                }
+            }
+            if (i < dialogueText.Length)
+            {
+                GM.dialoguePanel.GetComponentInChildren<Text>().text = dialogueText[i];
+            }
+            if (i == dialogueText.Length)
+            {
+                GM.dialoguePanel.GetComponentInChildren<Text>().text = dialogueText[dialogueText.Length - 1];
+            }
+
+        }
+
+        if (GameManeger.openTutor && openT)
+        {
+            GM.tutorPanel.GetComponentInChildren<Text>().text = openText;
+        }
+
+        if (enter)
+        {
+            if (Input.GetButtonUp("inBut") && !open)
+            {
+                open = true;
+                GameManeger.openDialogue = true;
+                GameManeger.openTutor = false;
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Character")
+        {
+            openT = true;
+            GameManeger.openTutor = true;
+            enter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        i = 0;
+        openT = false;
+        open = false;
+        GameManeger.openTutor = false;
+        GameManeger.openDialogue = false;
+        enter = false;
+    }
+
+
+}
--- Assets/Scripts/GameManeger.cs
+++ Assets/Scripts/GameManeger.cs
@@ -7,9 +7,18 @@ public class GameManeger : MonoBehaviour {
 
 	public static int coins;
 	public static bool dead;
+    public static bool openDialogue;
+    public static bool openTutor;
+    public GameObject tutorPanel;
+    public GameObject dialoguePanel;
 
 	void Start () {
-		
+
+        dialoguePanel = GameObject.Find("dialoguePanel");
+        tutorPanel = GameObject.Find("tutorPanel");
+        tutorPanel.SetActive(false);
+        dialoguePanel.SetActive(false);
+
 	}
 
 	void Update () {
@@ -19,5 +28,8 @@ public class GameManeger : MonoBehaviour {
 			SceneManager.LoadScene (2);
 			dead = false;
 		}
+
+        dialoguePanel.SetActive(openDialogue);
+        tutorPanel.SetActive(openTutor);
 	}
 }
