1a021a903993752ef3ff40b0c78afb8bff64d4b9
yalunar1 <yalunar1@gmail.com>
2018-05-14 19:28:29 +0300

    Light, flashlight and some minor changes

---
+++ Assets/Scripts/CursorImgFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorImgFollow : MonoBehaviour {
+
+    public float moveSpeed = 100f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), moveSpeed);
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotation_z = (Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z);
+
+    }
+}
