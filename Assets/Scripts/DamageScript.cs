e662e41164801c59f11a96c6e46f675ae5dd3288
Anton <antoha.svorets@mail.ru>
2018-05-17 19:55:18 +0300

    HP

---
+++ Assets/Scripts/DamageScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour {
+
+    public int damage;
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            HPScript.HP -= damage;
+        }
+    }
+}
